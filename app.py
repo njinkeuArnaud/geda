45cacd6fe756281a431dfd501ccdd865b3690541
njinkeuArnaud <149095018+njinkeuArnaud@users.noreply.github.com>
2025-06-15 07:35:50 +0100

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Chargement des utilisateurs depuis le fichier CSV
-def charger_utilisateurs():
-    return pd.read_csv("utilisateurs.csv")
-
-# Appel de la fonction
-utilisateurs = charger_utilisateurs()
