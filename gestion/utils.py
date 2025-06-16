ed4f203b04308d79f26c85e3aa140ae381f63ebc
njinkeuArnaud <149095018+njinkeuArnaud@users.noreply.github.com>
2025-06-16 09:31:41 +0100

    utils.py

--- gestion/utils.py
+++ gestion/utils.py
@@ -1,9 +1,12 @@
+# gestion/utils.py
+
 import qrcode
 import os
 
 def generer_qr_code(data: str, fichier_sortie: str):
     img = qrcode.make(data)
-    os.makedirs("data/qrcodes", exist_ok=True)
-    chemin = os.path.join("data/qrcodes", fichier_sortie)
+    dossier = "data/qrcodes"
+    os.makedirs(dossier, exist_ok=True)
+    chemin = os.path.join(dossier, fichier_sortie)
     img.save(chemin)
     return chemin
